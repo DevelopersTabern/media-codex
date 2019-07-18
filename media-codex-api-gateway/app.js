6447519163ed4bceef318ec02f72735c6bbbe8d5
Daniel Ramos <dramos@redd.cl>
2019-07-18 15:52:53 -0400

    Cambio de DNS de proxies a microservicios

--- media-codex-api-gateway/app.js
+++ media-codex-api-gateway/app.js
@@ -9,248 +9,244 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
-const userServiceProxy = httpProxy('http://192.168.99.100:27001');
+const userServiceProxy = httpProxy('ms-users:27001');
 
-const mediaServiceProxy = httpProxy('http://192.168.99.100:27002');
+const mediaServiceProxy = httpProxy('ms-media:27002');
 
-const generalServiceProxy = httpProxy('http://192.168.99.100:27003');
-
+const generalServiceProxy = httpProxy('ms-general:27003');
 
 
 
 app.post('/auth', (req, res) => {
-    const authBody = req.body;
+  const authBody = req.body;
 
-    // CONSULTA EN BASE DE DATOS
+  // CONSULTA EN BASE DE DATOS
 
-    //EXISTE 
-    jwt.sign(authBody, 'supersecreta', {}, (err, token) => {
-        if (err) {
-            console.log(err);
-            return res.send({ msg: 'error', error: err })
-        }
+  // EXISTE
+  jwt.sign(authBody, 'supersecreta', {}, (err, token) => {
+    if (err) {
+      console.log(err);
+      return res.send({msg: 'error', error: err})
+    }
 
-        return res.send({ token: token });
-    });
+    return res.send({token: token});
+  });
 
-    //NO EXISTE
-
-
+  // NO EXISTE
 })
 
 app.post('/waifus', (req, res) => {
-    const token = req.body.token;
+  const token = req.body.token;
 
-    jwt.verify(token, 'supersecreta', {}, (err, decoded) => {
-        if (err) {
-            return res.send({ msg: 'Tu, no estas autenticado', err });
-        }
+  jwt.verify(token, 'supersecreta', {}, (err, decoded) => {
+    if (err) {
+      return res.send({msg: 'Tu, no estas autenticado', err});
+    }
 
-        return res.send('waifus');
-    })
-
+    return res.send('waifus');
+  })
 })
 
 
 
-//MS-USER
+// MS-USER
 app.get('/user', (req, res, next) => {
-    userServiceProxy(req, res, next);
+  userServiceProxy(req, res, next);
 })
 
 app.get('/user/:id', (req, res, next) => {
-    userServiceProxy(req, res, next);
+  userServiceProxy(req, res, next);
 })
 
 app.post('/user', (req, res, next) => {
-    userServiceProxy(req, res, next);
+  userServiceProxy(req, res, next);
 })
 
 app.put('/user/:id', (req, res, next) => {
-    userServiceProxy(req, res, next);
+  userServiceProxy(req, res, next);
 })
 
 app.delete('/user/:id', (req, res, next) => {
-    userServiceProxy(req, res, next);
+  userServiceProxy(req, res, next);
 })
 
 
-//MS-USER
+// MS-USER
 app.get('/media', (req, res, next) => {
-    mediaServiceProxy(req, res, next);
+  mediaServiceProxy(req, res, next);
 })
 
 app.get('/media/:id', (req, res, next) => {
-    mediaServiceProxy(req, res, next);
+  mediaServiceProxy(req, res, next);
 })
 
 app.post('/media', (req, res, next) => {
-    mediaServiceProxy(req, res, next);
+  mediaServiceProxy(req, res, next);
 })
 
 app.put('/media/:id', (req, res, next) => {
-    mediaServiceProxy(req, res, next);
+  mediaServiceProxy(req, res, next);
 })
 
 app.delete('/media/:id', (req, res, next) => {
-    mediaServiceProxy(req, res, next);
+  mediaServiceProxy(req, res, next);
 })
 
 
-//MS-CASTING
+// MS-CASTING
 app.get('/casting', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/casting/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/casting', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/casting/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/casting/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 
-//MS-COMPANIES
+// MS-COMPANIES
 app.get('/companies', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/companies/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/companies', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/companies/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/companies/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 
-//MS-COUNTRIES
+// MS-COUNTRIES
 app.get('/countries', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/countries/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/countries', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/countries/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/countries/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 
 
-//MS-LANGUAGES
+// MS-LANGUAGES
 app.get('/languages', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/languages/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/languages', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/languages/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/languages/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 
-//MS-MEDIATYPE
+// MS-MEDIATYPE
 app.get('/mediaType', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/mediaType/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/mediaType', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/mediaType/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/mediaType/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
-//MS-MOVIEGENRE
+// MS-MOVIEGENRE
 app.get('/movieGenre', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/movieGenre/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/movieGenre', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/movieGenre/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/movieGenre/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 // MS-ROLES
 app.get('/roles', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.get('/roles/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.post('/roles', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.put('/roles/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 app.delete('/roles/:id', (req, res, next) => {
-    generalServiceProxy(req, res, next);
+  generalServiceProxy(req, res, next);
 })
 
 var server = http.createServer(app);
