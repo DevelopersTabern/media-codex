d98ed97fe4c99b4687cefa48d9defbb388532b97
Hans <dev.herzfelder@gmail.com>
2019-07-22 12:22:15 +0200

    Cambios a la base de datos

--- media-codex-devops/MediaCodex.sql
+++ media-codex-devops/MediaCodex.sql
@@ -8,55 +8,114 @@ CREATE TABLE User(
 	address VARCHAR(15) NOT NULL,
 PRIMARY KEY(id));
 
+
 CREATE TABLE Companies(
-	id INT AUTO_INCREMENT,
+	id INT,
 	name VARCHAR(15) NOT NULL,
 PRIMARY KEY(id));
 
-CREATE TABLE MovieGenre(
-	id INT AUTO_INCREMENT,
+
+CREATE TABLE Genre(
+	id INT,
 	name VARCHAR(15) NOT NULL,
 PRIMARY KEY(id));
+
 
 CREATE TABLE Languages(
-	id VARCHAR(10),
+	iso_639_1 VARCHAR(15),
 	name VARCHAR(15) NOT NULL,
-PRIMARY KEY(id));
+PRIMARY KEY(iso_639_1));
+
 
 CREATE TABLE Countries(
-	id VARCHAR(10),
+	iso_3166_1 VARCHAR(15),
 	name VARCHAR(15) NOT NULL,
-PRIMARY KEY(id)); 
+PRIMARY KEY(iso_3166_1)); 
+
 
 CREATE TABLE Roles(
 	id INT AUTO_INCREMENT,
 	name VARCHAR(15) NOT NULL,
 PRIMARY KEY(id));
 
+
 CREATE TABLE Casting(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT,
 	firstName VARCHAR(15) NOT NULL,
 	lastName VARCHAR(15) NOT NULL,
-	idRoles INT,
-FOREIGN KEY(idRoles) REFERENCES Roles(id));
+PRIMARY KEY(id));
 
-CREATE TABLE MediaType(
+
+CREATE TABLE Type(
 	id INT AUTO_INCREMENT,
 	name VARCHAR(15) NOT NULL,
 PRIMARY KEY(id));
 
+
 CREATE TABLE Media(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT,
 	name VARCHAR(15) NOT NULL,
-	idGenre INT,
-	idCompany INT,
-	idLanguage VARCHAR(10),
-	idCountry VARCHAR(10),
-	idCasting INT,
-	idMediaType INT,
-	FOREIGN KEY(idGenre) REFERENCES MovieGenre(id),
-	FOREIGN KEY(idCompany) REFERENCES Companies(id),
-	FOREIGN KEY(idLanguage) REFERENCES Languages(id),
-	FOREIGN KEY(idCountry) REFERENCES Countries(id),
-	FOREIGN KEY(idCasting) REFERENCES Casting(id),
-	FOREIGN KEY(idMediaType) REFERENCES MediaType(id));
+PRIMARY KEY(id));
+    
+CREATE TABLE Rating(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	idMedia INT,
+    idUser INT,
+    rating INT NOT NULL,
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(idUser) REFERENCES User(id));
+
+
+CREATE TABLE RolesCast(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idRoles INT,
+    idCast INT,
+FOREIGN KEY(idRoles) REFERENCES Roles(id),
+FOREIGN KEY(idCast) REFERENCES Casting(id));
+
+
+CREATE TABLE MediaCountry(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idMedia INT,
+    id_iso_3166_1 VARCHAR(15),
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(id_iso_3166_1) REFERENCES Countries(iso_3166_1));
+
+CREATE TABLE MediaCast(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idMedia INT,
+    idCast INT,
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(idCast) REFERENCES Casting(id));
+
+
+CREATE TABLE MediaType(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idMedia INT,
+    idType INT,
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(idType) REFERENCES Type(id));
+    
+
+CREATE TABLE MediaCompany(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idMedia INT,
+    idCompany INT,
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(idCompany) REFERENCES Companies(id));
+
+
+CREATE TABLE MediaLanguage(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idMedia INT,
+    id_iso_639_1 VARCHAR(15),
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(id_iso_639_1) REFERENCES Languages(iso_639_1));
+
+
+CREATE TABLE MediaGenre(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    idMedia INT,
+    idGenre INT,
+FOREIGN KEY(idMedia) REFERENCES Media(id),
+FOREIGN KEY(idGenre) REFERENCES Genre(id));
