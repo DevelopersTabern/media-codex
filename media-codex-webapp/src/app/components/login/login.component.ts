be8316ccad72ee2423f7c0155556967ba37bf787
Daniel Ramos <dev.rrad@gmail.com>
2019-08-03 16:02:05 -0400

    Mensaje de alerta en caso de problemas de login

--- media-codex-webapp/src/app/components/login/login.component.ts
+++ media-codex-webapp/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -14,18 +14,25 @@ export class LoginComponent implements OnInit {
   email: string;
   password: string;
 
+  error: string;
+
   constructor(private httpClient: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
-  
+
   authenticate(email: string, password: string) {
     const userBody = { email: email, password: password };
 
     this.httpClient.post('http://localhost:27100/auth', userBody).subscribe((response: any) => {
-      this.token = response.token
-      this.router.navigate(['app/medias-list'])
-    });
+      if (response.token) {
+        this.token = response.token;
+        this.router.navigate(['app/medias-list']);
+      }
+      else {
+        this.error = 'Usuario y/o contraseña invalidos';
+      }
+    }, (err) => this.error = 'Error al comunicarse con el servidor');
 
   }
 }
