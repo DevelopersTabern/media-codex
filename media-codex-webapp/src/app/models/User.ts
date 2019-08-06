497fa47729cbd05a8be9f36915182980e4565914
Hans <dev.herzfelder@gmail.com>
2019-08-06 10:21:20 +0200

    modelo y servicio User

---
+++ media-codex-webapp/src/app/models/User.ts
@@ -0,0 +1,11 @@
+import { Title } from '@angular/platform-browser';
+
+export interface User {
+    id?: number;
+    userName?: string;
+    password: string;
+    fisrtName: string;
+    lastName: string;
+    email: string;
+    address: string;
+};
--- media-codex-webapp/src/app/services/users.service.ts
+++ media-codex-webapp/src/app/services/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  API_URI = 'http://localhost:27100';
+
+  constructor(private http: HttpClient) { }
+
+  saveUser(user: User){
+    return this.http.post(`${this.API_URI}/user`, user);
+  }
 }
