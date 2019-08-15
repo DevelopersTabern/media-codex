a79d0a6d76452f8be0c83c7c3d83a8efd7fbe649
Daniel Ramos <dev.rrad@gmail.com>
2019-08-15 12:43:08 -0400

    Rutas para generos y lenguajes añadidos a gateway

--- media-codex-api-gateway/app.js
+++ media-codex-api-gateway/app.js
@@ -13,49 +13,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use(require('./routes/user'));
-
 app.use(require('./routes/media'));
-
 app.use(require('./routes/roles'));
-
 app.use(require('./routes/casting'));
-
 app.use(require('./routes/companies'));
-
 app.use(require('./routes/countries'));
-
 app.use(require('./routes/mediaType'));
-
-app.use(require('./routes/movieGenre'));
+app.use(require('./routes/genre'));
+app.use(require('./routes/languages'));
 
 
 app.post('/auth', (req, res) => {
   const authBody = req.body;
   connection.query('SELECT * FROM User WHERE email = ?',[authBody.email], function(err,results){
     if(err){
-      
       console.log(err);
       return res.send({msg: 'error', error: err})
-    
     }else{
-      
       if(results.length > 0){
-
         if(authBody.password == results[0].password){
           jwt.sign(authBody, 'supersecreta', {}, (err,token) => {
-
             return res.send({token: token});
-    
           });
         }else{
-
           res.send({msg: 'clave invalida', error: err})
-
         }
       }else{
-
         res.send({msg: 'El correo no existe', error: err})
-        
       }
     }
   })  
@@ -68,7 +52,6 @@ app.post('/waifus', (req, res) => {
     if (err) {
       return res.send({msg: 'Tu, no estas autenticado', err});
     }
-
     return res.send('waifus');
   })
 })
